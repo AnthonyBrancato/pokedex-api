654b520138cd8bfabda5a52f490e7bb056e5e8b4
abrancato <abrancato@abrancato.home>
2021-06-19 20:30:12 +0200

    refactor fetch

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const logger = require("morgan");
 const fetch = require("node-fetch");
@@ -6,35 +7,32 @@ const app = express();
 
 const PORT = process.env.PORT || 3200;
 
-const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+// const API_URL = process.env.API_URL;
+const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 const firstGen = 151;
 
 app.use(logger("dev"));
 
 app.get("/create-first-gen", (req, res) => {
-
-  fetch(`${API_URL}?limit=${firstGen}`)
-    .then(response => response.json())
-    .then(data => {
-      let results = data.results.map(item => item.url)
-
-      for(let i = 0; i < results.length; i++) {
-        fetch(`${API_URL}${i}`)
-          .then(response => response.json())
-          .then(data => {
-            fs.writeFileSync(`./data/${data.name}.json`, JSON.stringify(data))
-            res.end()
-          })
-          .catch(err => { console.error(err); })
-      }
-
-    })
-    .catch(err => { console.error(err); })
+  for(let i = 0; i < firstGen; i++) {
+    fetch(`${API_URL}${i}`)
+      .then(response => response.json())
+      .then(data => {
+        let pokemonObj = {
+          name: data.name,
+          id: data.id,
+          sprites: data.sprites
+        }
+        fs.writeFileSync(`./data/first-generation/${data.name}.json`, JSON.stringify(pokemonObj))
+        res.end()
+      })
+      .catch(err => { console.error(err); })
+  }
     res.end()
 });
 
 app.get('/pokemons', (req,res) => {
-  console.log(req);
+  res.end()
 })
 
 app.get('/pokemons/:name', (req, res) => {
